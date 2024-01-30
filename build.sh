c3c7891d87c78ac7991016a2d72b5d3267305b59
Ash <61899730+exyorha@users.noreply.github.com>
2024-01-30 14:49:15 +0300

    Code organization cleaned up.

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 set -e
 cmake \
-    -S il2cpp-arm-adapter \
-    -B build -G "Kate - Ninja" \
+    -S CompatibilityRuntime \
+    -B CompatibilityRuntime-build -G "Kate - Ninja" \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=TRUE \
     -DCMAKE_BUILD_TYPE=RelWithDebInfo \
     -DCMAKE_INSTALL_PREFIX="$(realpath -- graft)"
-ln -sf ../build/compile_commands.json il2cpp-arm-adapter
-cmake --build build
-cmake --install build --component GameAssembly
-
+ln -sf ../CompatibilityRuntime-build/compile_commands.json CompatibilityRuntime
+cmake --build CompatibilityRuntime-build
+cmake --install CompatibilityRuntime-build --component GameAssembly
 
 abi=arm64-v8a
 ANDROID_NDK_VERSION=21.4.7075529
