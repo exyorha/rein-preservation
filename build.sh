8269127bf408cd57fbc20f475ae49e3079bc7638
Ash <61899730+exyorha@users.noreply.github.com>
2024-02-09 14:46:58 +0300

    Remove LogAES.
    It wasn't ever used and doesn't actually work anyway.

--- build.sh
+++ build.sh
@@ -8,15 +8,3 @@ cmake \
 ln -sf ../CompatibilityRuntime-build/compile_commands.json CompatibilityRuntime
 cmake --build CompatibilityRuntime-build
 cmake --install CompatibilityRuntime-build --component GameAssembly
-
-abi=arm64-v8a
-ANDROID_NDK_VERSION=21.4.7075529
-ANDROID_PLATFORM=21
-
-cmake -S LogAES -B LogAES-build$abi -DCMAKE_TOOLCHAIN_FILE=/opt/android-sdk/ndk/${ANDROID_NDK_VERSION}/build/cmake/android.toolchain.cmake \
-    -DANDROID_ABI=$abi \
-    -DANDROID_PLATFORM=android-${ANDROID_PLATFORM} \
-    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-    -DCMAKE_INSTALL_PREFIX=${PWD}/replacement-apk-files
-
-cmake --build LogAES-build$abi --target install/strip
